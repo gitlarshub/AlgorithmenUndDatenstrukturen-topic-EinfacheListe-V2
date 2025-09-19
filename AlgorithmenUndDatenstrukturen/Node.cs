27e27846ea0b51f478c000453f02d91c1fb06050
Lars <lars.veljaca1@icloud.com>
2025-09-19 09:43:33 +0200

    FIx

---
+++ AlgorithmenUndDatenstrukturen/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace CommonDLL
+{
+    public class Node
+    {
+        public Person Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(Person data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
---
+++ DatastructureDLL/SingleLinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Xml.Linq;
+using CommonDLL;
+
+namespace DataStructures
+{
+    public class SingleLinkedList
+    {
+        private Node head;
+
+        public SingleLinkedList()
+        {
+            head = null;
+        }
+
+        public void Add(Person person)
+        {
+            Node newNode = new Node(person);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public bool Contains(Person person)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(person))
+                {
+                    return true;
+
+
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+    }
+}
